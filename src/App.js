86f3322482d2b91a7e522dd80195708a43319761
Hardie-stuff <lara.hardie.stewart@gmail.com>
2021-09-17 13:51:10 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lara Hardie and is
-        <a
-          href="https://github.com/Hardie-stuff/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Lara Hardie and is
+          <a
+            href="https://github.com/Hardie-stuff/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
