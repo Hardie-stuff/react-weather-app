46f94c61efbc95731ffdf82b05fb196110f6cbdd
Hardie-stuff <lara.hardie.stewart@gmail.com>
2021-09-17 13:41:57 +0200

    Added footer

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lara Hardie and is
+        <a
+          href="https://github.com/Hardie-stuff/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
