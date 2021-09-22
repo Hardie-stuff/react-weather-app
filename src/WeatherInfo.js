24145fbdea0f92bc616769daafe273b6bdc94b86
Hardie-stuff <lara.hardie.stewart@gmail.com>
2021-09-22 17:49:08 +0200

    Added forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,6 +4,7 @@ import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
+  console.log(props.data);
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
@@ -30,7 +31,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Wind: {props.data.wind}km/h</li>
           </ul>
         </div>
       </div>
